887a01d7ceec142c98b38a0eb5fe22fffdc7185e
Karlkim Suwamongkol <karlkim@outlook.com>
2014-02-22 13:06:53 -0600

    Add get tweets (statuses) with screen name

--- src/Twitter/Search.fs
+++ src/Twitter/Search.fs
@@ -5,6 +5,8 @@ open System.Linq
 open LinqToTwitter
 open TwitterContext
 
+let s2ul (s : string) = Convert.ToUInt64(s)
+
 // get search result
 let getSearchResult q num = 
     query { 
@@ -30,7 +32,6 @@ let getSearchResultWithMaxId q num maxId =
 
 // get statuses from number of batches
 let getStatuses q num batches = 
-    let s2ul (s : string) = Convert.ToUInt64(s)
     
     let getStatuses q maxId = 
         (getSearchResultWithMaxId q num maxId).Statuses
@@ -48,3 +49,42 @@ let getStatuses q num batches =
         (getStatuses q maxId) @ acc
     
     [ 0..batches ] |> List.fold combinedStatuses []
+
+// get tweets by screen name from number of batches
+let getTweetsByScreenName screenName batches = 
+        
+    let getTweets maxId = 
+        let tweets = 
+            if maxId = UInt64.MaxValue then
+                query{
+                    for tweet in ctx.Status do
+                        where (tweet.Type = StatusType.User)
+                        where (tweet.ScreenName = screenName)
+                        where (tweet.Count = 200)                        
+                        select tweet
+                }
+            else
+                query{
+                    for tweet in ctx.Status do
+                        where (tweet.Type = StatusType.User)
+                        where (tweet.ScreenName = screenName)
+                        where (tweet.Count = 200)
+                        where (tweet.MaxID = maxId)
+                        select tweet
+                }
+
+        tweets
+        |> List.ofSeq
+        |> List.rev
+    
+    let getAllTweets (acc : Status list) _ = 
+        let maxId = 
+            if acc = [] then UInt64.MaxValue
+            else 
+                (acc
+                 |> List.head
+                 |> (fun s -> s.StatusID)
+                 |> s2ul) - 1UL
+        (getTweets maxId) @ acc
+    
+    [ 0..batches ] |> List.fold getAllTweets []
