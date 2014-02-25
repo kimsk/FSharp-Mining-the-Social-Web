efeaee6bed6f6e811df32de6b5fd662530cc429f
Karlkim Suwamongkol <karlkim@outlook.com>
2014-02-24 19:55:46 -0600

    Play more with Dynamic Linq in F#

---
+++ src/Twitter/Status.fs
@@ -0,0 +1,26 @@
+﻿module Status
+
+open System
+open System.Linq
+open LinqToTwitter
+open TwitterContext
+open Microsoft.FSharp.Linq.RuntimeHelpers
+
+let buildSearchQuery statusType (filters:(Status->bool) list) = 
+    let status = ctx.Status.Where(fun s -> s.Type = statusType)
+
+    let expr f = 
+        LeafExpressionConverter
+            .QuotationToLambdaExpression(<@ Func<_, _>(f)@>)
+        
+    filters |> List.fold (fun (q:IQueryable<Status>) f -> q.Where(expr f)) status
+
+[
+    (fun (s:Status) -> s.ScreenName = "kimsk")
+    (fun s -> s.Count = 10)
+] 
+    |> buildSearchQuery StatusType.User 
+    |> Array.ofSeq
+
+buildSearchQuery StatusType.User []
+    |> Array.ofSeq
